b053763dfb64b595b859ad09fa9b2addb090c43b
Ridho Satria Magnus <35208046+ridho-satria-magnus@users.noreply.github.com>
2018-01-08 10:54:44 +0700

    Update modal-oop.js

--- modal-oop.js
+++ modal-oop.js
@@ -1,15 +1,15 @@
 var Jconfirm = {
-    title: 'Modal Title',
-  msg: 'This is your message of confirmation',
-  button1: 'Yes',
-  button2: 'No',
-  action1: 'alert("Hello this is action 1")',
-  action2: 'alert("Hello this is action 2")',
-  modalTitle: $(".title-confirm"),
-  body: $(".modal-confirm"),
-  btn1: $(".btn1-confirm"),
-  btn2: $(".btn2-confirm"),
-  show: function(){
+    title: 'Modal Titlxze',
+    msg: 'This is your message of concvsdsfirmation',
+    button1: 'Yes',
+    button2: 'No',
+    action1: 'alert("Hello this is action 1")',
+    action2: 'alert("Hello this is action 2")',
+    modalTitle: $("#modal-title"),
+    body: $("#modal-body"),
+    btnDismiss: $("#modal-btn-dismiss"),
+    btnAccept: $("#modal-btn-accept"),
+    show: function(){
     this.body.html(this.msg);
     this.btn1.html(this.button1);
     this.btn2.html(this.button2);
@@ -20,9 +20,10 @@ var Jconfirm = {
         title: data.title || Jconfirm.title,
         msg: data.msg || Jconfirm.msg,
         do: function(){
-            Jconfirm.body.innerHTML = this.msg;
+            Jconfirm.modalTitle.html(this.title);
+            Jconfirm.body.html(this.msg);
       }
-    })
+    });
     return obj.do();
   },
   exec1: function(){
@@ -32,9 +33,11 @@ var Jconfirm = {
     eval(this.action2);
   }
 }
-​
-var konfirmasi = Object.create(Jconfirm);
-konfirmasi.showErik({
-    title: "Erik Modal",
-    msg: "Messages from Erik"
-})
+
+function alerting() {
+    var konfirmasi = Object.create(Jconfirm);
+    konfirmasi.showErik({
+        title: "Erik Modal",
+        msg: "Messages from Erik"
+    })
+}
